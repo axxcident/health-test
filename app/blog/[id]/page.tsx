c9f7951f28debe27f2150aeacb92f52c3ed9df49
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2024-01-23 12:12:05 +0100

    tutorial över

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,11 +1,43 @@
-export default function Blog ({ params }: {
+
+type Time = {
+  datetime: string
+}
+
+export async function generateMetadata({ params }: {
+  params: {id: string}
+  }) {
+    return {
+      title: "my blog post",
+      description: `blog post for number ${params.id}`,
+    };
+  }
+
+export default async function Blog ({ params }: {
   params: {id: string}
   })
 
   {
+
+
+  const response = await fetch('https://worldtimeapi.org/api/timezone/Europe/Stockholm',
+    {
+      cache: "no-store"
+    },
+    // {
+    // method: 'GET',
+    // headers: {
+    //   'Content-Type': 'application/json'
+    //   }
+    // }
+    )
+
+    const data: Time = await response.json()
+
+
   return (
     <main className="">
         <h1>Blog ID: {params.id}</h1>
+        <p>TimeData: {data.datetime}</p>
     </main>
   );
 };
