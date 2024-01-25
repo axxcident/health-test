34c5d7ce024c63aff2053108992bb5859931e372
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2024-01-25 09:16:16 +0100

    uppdaterade vercel

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="">
-        <h1>Hello world testar igen</h1>
+        <h1>Hello world testar igen2</h1>
     </main>
   );
 }
